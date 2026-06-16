7de76378cf7427a5e9c99e7b783c3a3ed1d7a18a
BRYAN NONATO DA SILVA CARVALHO <bryan.carvalho5@senaisp.edu.br>
2026-06-16 12:32:14 -0300

    mudando .env

--- src/models/Categoria.js
+++ src/models/Categoria.js
@@ -46,8 +46,8 @@ export class Categoria{
         }
     }
     #validarDescricao(value) {
-        if(value && (value.trim().length < 10 || value.trim().length > 100)){
-            throw new Error ('O campo descrição e deve ter entre 10 a 100 caracteres');           
+        if(!value || value.trim().length < 10 || value.trim().length > 100 ){
+            throw new Error ('O campo descrição é obrigatório e deve ter entre 10 a 100 caracteres');           
         }
     }
 
